e77ab9a4c6528b341d60aa4bf8ceca53f08e2a4f
fikri <fikritaufiq21@gmail.com>
2024-07-09 21:57:03 +0700

    Pekan4-Tugas2 Javascript

---
+++ tugas-frontend/tugas-javascript-dasar/Function.js
@@ -0,0 +1,30 @@
+// Soal Function
+// Jawaban Soal 1
+
+function teriak() {
+    return "Halo Sanbers!";
+  }
+  
+  console.log(teriak());
+
+
+
+// Soal Function
+// Jawaban Soal 2
+
+function kalikan(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(kalikan(4, 12));
+
+
+
+// Soal Function
+// Jawaban Soal 3
+
+function introduce(name, age, address, hobby) {
+  return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!";
+}
+
+console.log(introduce("Agus", 30, "Jogja", "Gaming"));
---
+++ tugas-frontend/tugas-javascript-dasar/String.js
@@ -0,0 +1,62 @@
+// Soal String
+// Jawaban Soal 1
+console.log('                            ')
+console.log('Jawaban no.1:')
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
+
+
+
+// Soal String
+// Jawaban Soal 2
+console.log('                            ')
+console.log('Jawaban no.2:')
+
+var sentence = "I am going to be Web Developer";
+
+var exampleFirstWord = sentence.substring(0, 1);
+var secondWord = sentence.substring(2, 4);
+var thirdWord = sentence.substring(5, 11);
+var fourthWord = sentence.substring(11, 14);
+var fifthWord = sentence.substring(14, 17);
+var sixthWord = sentence.substring(17, 21);
+var seventhWord = sentence.substring(22, 31);
+var eighthWord = sentence.substring(21, 31);
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+console.log('Sixth Word: ' + sixthWord);
+console.log('Seventh Word: ' + seventhWord);
+console.log('Eighth Word: ' + eighthWord);
+
+
+
+// Soal String
+// Jawaban Soal 3
+console.log('                            ')
+console.log('Jawaban no.3:')
+
+var sentence3 = 'wow JavaScript is so cool';
+
+var exampleFirstWord3 = sentence3.substring(0, 3);
+var secondWord3 = sentence3.substring(4, 14);
+var thirdWord3 = sentence3.substring(15, 17);
+var fourthWord3 = sentence3.substring(18, 20);
+var fifthWord3 = sentence3.substring(21, 25);
+
+console.log('First Word: ' + exampleFirstWord3 + ', with length: ' + exampleFirstWord3.length);
+console.log('Second Word: ' + secondWord3 + ', with length: ' + secondWord3.length);
+console.log('Third Word: ' + thirdWord3 + ', with length: ' + thirdWord3.length);
+console.log('Fourth Word: ' + fourthWord3 + ', with length: ' + fourthWord3.length);
+console.log('Fifth Word: ' + fifthWord3 + ', with length: ' + fifthWord3.length);
+console.log('                            ')
