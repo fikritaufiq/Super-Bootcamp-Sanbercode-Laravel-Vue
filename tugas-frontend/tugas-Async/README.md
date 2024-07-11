b4cc2750ae11658538693841b499d5f8b42cf41c
fikri <fikritaufiq21@gmail.com>
2024-07-12 02:15:47 +0700

    Pekan4-Tugas4 ES6 & Async

---
+++ tugas-frontend/tugas-Async/README.md
@@ -0,0 +1,13 @@
+# IPLEMENTASI PROJECT:
+
+Buat project npm baru dengan perintah: npm init
+
+Install module Babel seperti berikut : npm install @babel/core @babel/cli @babel/preset-env core-js --save
+
+Membuat folder dist : npm run build
+
+Release 0 Register Karyawan (Callback) : node dist register Bondra,123456,admin
+
+Release 0 Register Karyawan (Callback) : node dist login Bondra,123456
+
+Release 0 Register Karyawan (Callback) : node dist addSiswa Regy,Iqbal
---
+++ tugas-frontend/tugas-Async/src/index.js
@@ -0,0 +1,38 @@
+const register = require('./lib/register');
+const login = require('./lib/login');
+const addSiswa = require('./lib/addSiswa');
+
+const command = process.argv[2];
+const input = process.argv.slice(3).join(',');
+
+switch (command) {
+  case 'register':
+    register(input, (err, result) => {
+      if (err) {
+        console.error('Error:', err);
+      } else {
+        console.log(result);
+      }
+    });
+    break;
+  case 'login':
+    login(input, (err, result) => {
+      if (err) {
+        console.error('Error:', err);
+      } else {
+        console.log(result);
+      }
+    });
+    break;
+  case 'addSiswa':
+    addSiswa(input, (err, result) => {
+      if (err) {
+        console.error('Error:', err);
+      } else {
+        console.log(result);
+      }
+    });
+    break;
+  default:
+    console.log('Command not recognized');
+}
