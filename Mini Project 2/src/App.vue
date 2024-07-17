f7618449500bd0736a6b1180dd70873fad1c2abf
fikri <fikritaufiq21@gmail.com>
2024-07-17 23:51:40 +0700

    update untuk mini project 2

---
+++ Mini Project 2/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="wrapper">
+    <Navbar @scrollTo="scrollTo" />
+    <div class="content mt-16">
+      <Home ref="homeSection" />
+      <Skills ref="skillsSection" />
+      <Portfolio ref="portfolioSection" />
+      <Experience ref="experienceSection" />
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Navbar from './components/Navbar.vue';
+import Home from './components/Home.vue';
+import Skills from './components/Skills.vue';
+import Portfolio from './components/Portfolio.vue';
+import Experience from './components/Experience.vue';
+import Footer from './components/Footer.vue';
+
+const homeSection = ref(null);
+const skillsSection = ref(null);
+const portfolioSection = ref(null);
+const experienceSection = ref(null);
+
+const scrollTo = (section) => {
+  const element = {
+    homeSection,
+    skillsSection,
+    portfolioSection,
+    experienceSection,
+  }[section];
+  if (element.value) {
+    element.value.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+</script>
+
+<style scoped>
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background: linear-gradient(to bottom, #3123b2, #9ec2db); /* Gradient dari atas (#2d0f47) ke bawah (#6096ba) */
+}
+
+.content {
+  flex: 1;
+}
+
+.section {
+  margin-bottom: 2rem;
+  color: black; /* Sesuaikan warna teks jika diperlukan */
+  background-color: #9ec2db;
+  max-width: 1000px; /* Atur lebar maksimal sesuai kebutuhan */
+  margin-left: auto;
+  margin-right: auto;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+/* Tambahkan gaya responsif sesuai kebutuhan */
+</style>
---
+++ Mini Project 2/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="bg-black p-4 p-4 fixed top-0 left-0 right-0 z-10 flex justify-between items-center">
+    <div class="text-white font-bold">My Portfolio</div>
+    <ul class="flex space-x-4">
+      <li @click="handleScroll('homeSection')" class="text-white font-bold cursor-pointer">Home</li>
+      <li @click="handleScroll('skillsSection')" class="text-white font-bold cursor-pointer">Skills</li>
+      <li @click="handleScroll('portfolioSection')" class="text-white font-bold cursor-pointer">Portfolio</li>
+      <li @click="handleScroll('experienceSection')" class="text-white font-bold cursor-pointer">Experience</li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue';
+
+defineEmits(['scrollTo']);
+
+const handleScroll = (section) => {
+  emit('scrollTo', section);
+};
+</script>
