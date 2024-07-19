181fbfe7aab33df4a54ff68f649b77fe889c0898
fikri <fikritaufiq21@gmail.com>
2024-07-19 18:07:03 +0700

    Mini Project 2

---
+++ Mini Project 2/src/main.js
@@ -0,0 +1,16 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'primeicons/primeicons.css'
+import './styles/index.css'
+import { createPinia } from 'pinia'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+
+app.use(router)
+
+app.mount('#app')
